4e9db49ad617ebc24af69c1769b81683b313b855
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-03-11 13:59:54 -0400

    anchor tags each bio

--- front-end/src/Components/description/Engeneers.js
+++ front-end/src/Components/description/Engeneers.js
@@ -1,5 +1,7 @@
 import React from 'react'
-// import {Link} from 'react-router-dom';
+// import {BrowserRouter} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+
 
 function Engeneers(props){
     return (
@@ -17,16 +19,18 @@ function Engeneers(props){
                          bofeado quemacocos. tracalero fumigado. poca madre volado. guacarear fayuquero. 
                         pingas feliz como una lombriz.
                 </p>
-                <div className="bioLink">
-                    <i className="fas fa-globe"></i>
-                    <i className="fab fa-github fa-lg"></i>
-                    <i className="fab fa-instagram fa-lg"></i>
-                </div>
+            
+                    <div className="bioLink">
+                        <a href="https://www.loubrianlafuente.com/index.html" target="_blank"><i className="fas fa-globe fa-3x"></i></a>
+                        <a href="https://github.com/blafuente" target="_blank"><i className="fab fa-github fa-3x"></i></a>
+                        <a href="https://www.linkedin.com/in/brianlafuente11/" target="_blank"><i className="fab fa-linkedin-in fa-3x"></i></a>
+                    </div>
+
                 
             </div>
             <div class="codeImg">
                 <div className="codeBox">
-                    <img src="./Images/FullSizeRender4.png" alt=""/>
+                    <img src="./Images/hg_50.jpg" alt=""/>
                 </div>
             </div>
         </div>
@@ -45,9 +49,9 @@ function Engeneers(props){
                                     feliz como una lombriz.
             </p>
             <div class="bioLink">
-                    <i className="fas fa-globe"></i>
-                    <i className="fab fa-github fa-lg"></i>
-                    <i className="fab fa-instagram fa-lg"></i>
+                <a href="http://brandonhdez.com/" target="_blank"><i className="fas fa-globe fa-3x"></i></a>
+                <a href="https://github.com/brandonhdez7" target="_blank"><i className="fab fa-github fa-3x"></i></a>
+                <a href="https://www.linkedin.com/in/brandon-hernandez-34254b175/" target="_blank"><i className="fab fa-linkedin-in fa-3x"></i></a>
             </div>
             </div>
         </div>
@@ -60,9 +64,9 @@ function Engeneers(props){
                       bofeado quemacocos. tracalero fumigado. poca madre volado. guacarear fayuquero. pingas 
                       feliz como una lombriz.</p>
                       <div className="bioLink">
-                              <i className="fas fa-globe"></i>
-                              <i className="fab fa-github fa-lg"></i>
-                              <i className="fab fa-instagram fa-lg"></i>
+                        <a href="https://khanhqvu.com/" target="_blank"><i className="fas fa-globe fa-3x"></i></a>
+                        <a href="https://github.com/Khanhvu09" target="_blank"><i className="fab fa-github fa-3x"></i></a>
+                        <a href="https://www.linkedin.com/in/khanh-vu-75ba8394/" target="_blank"><i className="fab fa-linkedin-in fa-3x"></i></a>
                           </div>
             </div>
             <div className="codeImg">
